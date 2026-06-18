c92b506b213f42fbdf06c3b8dba4620b41059e31
Muhammadrehan92 <rehanyaqoob924@gmail.com>
2026-06-18 18:58:26 +0500

    Add priority

--- Script.js
+++ Script.js
@@ -11,10 +11,14 @@ function addTask() {
         return;
     }
 
+    let priority = document.getElementById("priority").value;
+
     tasks.push({
         text: task,
-        completed: false
+        completed: false,
+        priority: priority
     });
+
 
     localStorage.setItem("tasks", JSON.stringify(tasks));
 
@@ -27,7 +31,7 @@ function toggleCheckbox(index) {
     tasks[index].completed = !tasks[index].completed;
 
     localStorage.setItem("tasks", JSON.stringify(tasks));
- displayTasks();    
+    displayTasks();
 }
 
 
@@ -52,12 +56,35 @@ function displayTasks() {
     taskList.innerHTML = "";
 
     tasks.forEach((task, index) => {
+        let color = "";
+
+        if (task.priority === "High") {
+            color = "green";
+        }
+        else if (task.priority === "Medium") {
+            color = "orange";
+        }
+        else {
+            color = "red";
+        }
+
         taskList.innerHTML += `
             <li class="task">
                 <span class="task-text">${task.text}</span>
  
                 <span class="status">
     ${task.completed ? "Task Complete" : "Task Not Complete"}
+</span>
+<span
+style="
+background:${color};
+color:white;
+padding:5px 12px;
+border-radius:20px;
+font-size:13px;
+margin-left:10px;
+">
+${task.priority}
 </span>   
                 <div class="actions">
                 <input
